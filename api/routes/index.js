c33db6dcb7fce69c82bb3a8343f5f3fbbc9581e4
Marcosmm1 <marcos-ubuntu@Marcos.localdomain>
2020-04-07 16:21:57 +0100

    /me

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,13 +3,13 @@ const { authUser } = require('../utils/index')
 
 const authRouter = require('./auth.router')
 // const usersRouter = require('./user.router')
-// const clientsRouter = require('./clients.router')
+const clientsRouter = require('./clients.router')
 // const meRouter = require('./me.router')
 
 router.use('/auth', authRouter)
 // router.use('/me', meRouter)
 // router.use('/users', authUser, usersRouter)
-// router.use('/me/clients', authUser, clientsRouter)
+router.use('/me/clients', authUser, clientsRouter)
 
 // router.get('/whoami', authUser, (req, res) => {
 //   res.send(`hi there! ${res.locals.user.name}`)
