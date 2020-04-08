a8963aa02ae90918498b006f6316796cbbc01d3b
Marcosmm1 <marcos-ubuntu@Marcos.localdomain>
2020-04-08 01:57:30 +0100

    Descomented User

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,21 +2,18 @@ const router = require('express').Router()
 const { authUser } = require('../utils/index')
 
 const authRouter = require('./auth.router')
-// const usersRouter = require('./user.router')
+const usersRouter = require('./user.router')
 const clientsRouter = require('./clients.router')
 const productsRouter = require('./products.router')
 const ordersRouter = require('./orders.router')
 const categoriesRouter = require('./categories.router')
-// const meRouter = require('./me.router')
 
 router.use('/me/clients', authUser, clientsRouter)
 router.use('/auth', authRouter)
 router.use('/products', productsRouter)
 router.use('/categories', categoriesRouter)
 router.use('/orders', ordersRouter)
-// router.use('/products/:productId', productsRouter)
-// router.use('/me', meRouter)
-// router.use('/users', authUser, usersRouter)
+router.use('/users', authUser, usersRouter)
 
 // router.get('/whoami', authUser, (req, res) => {
 //   res.send(`hi there! ${res.locals.user.name}`)
